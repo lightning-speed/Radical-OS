8d354a0cf400039529a2e9cacf0661138784dfea
raxracks <multisniperism@gmail.com>
2021-08-25 18:21:14 +1200

    linux build script

--- build.sh
+++ build.sh
@@ -15,9 +15,7 @@ gcc -m32 -c -I ../src/. ../src/Terminal-Runtime/*.c -fno-stack-protector
 gcc -m32 -c -I ../src/. ../src/Util/*.c -fno-stack-protector
 ld -m elf_i386 ../kernel_loader.o ../kernel.o *.o -o kernel.bin -Ttext 0x1000
 rm -rf *.o
-rm -rf ../*.o
-#objcopy -Felf32-i386 kernel.e kernel.bin
-rm -rf *.e
+rm -rf ../*.rm -rf *.e
 qemu-system-x86_64 -kernel kernel.bin
 cd ..
 rm -rf bin
