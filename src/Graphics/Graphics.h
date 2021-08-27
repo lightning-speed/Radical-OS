92e680422be8c4408b70820665b56765bfbb120c
sheispeko <82322282+lightning-speed@users.noreply.github.com>
2021-08-27 18:14:32 +0530

    Add files via upload

---
+++ src/Graphics/Graphics.h
@@ -0,0 +1,5 @@
+#pragma once
+void setCharAt(int xe,int ye,char ae,int color);
+void printAt(int xe,int ye,char * tex,int color);
+void printCenter(int ye,char * text,int color);
+void setBlockAt(int x,int y,int color);
