2c890d5dba07fdb53333e4d83d23ff6e9628552a
sheispeko <82322282+lightning-speed@users.noreply.github.com>
2021-09-09 15:39:32 +0530

    Delete src directory

--- src/Graphics/Graphics.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-void setCharAt(int xe,int ye,char ae,int color);
-void printAt(int xe,int ye,char * tex,int color);
-void printCenter(int ye,char * text,int color);
-void setBlockAt(int x,int y,int color);
-void makeWindow(char * window_name);
-void printStringInWindow(char * text);
-void start_window_system();
