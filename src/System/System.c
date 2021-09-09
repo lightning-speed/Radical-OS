be15cf0a0c9bec157d72e38f34d88886e7b4bff9
sheispeko <82322282+lightning-speed@users.noreply.github.com>
2021-09-09 15:42:06 +0530

    Add files via upload

---
+++ src/System/System.c
@@ -0,0 +1,12 @@
+#include "System.h"
+#include <Drivers/Screen.h>
+#include<System/IO.h>
+void restart(){
+     uint8 good = 0x02;
+     while (good & 0x02)
+     good = inportb(0x64);
+     outportb(0x64, 0xFE);
+}
+void shutdown(){
+     __asm__ __volatile__("int $0x10");
+}
