aa4249a1a5f7ae739173cb048c36a96206d04990
sheispeko <82322282+lightning-speed@users.noreply.github.com>
2021-09-20 15:27:21 +0530

    Add files via upload

---
+++ src/FS/default_files.h
@@ -0,0 +1,8 @@
+void createNativeFiles(){
+createFile("hello");
+writeFile("hello","movn af 6\ncmdn 4 1\nmovn ac 'H\ncmdn 1 1\nmovn ac 'e\ncmdn 1 1\nmovn ac 'l\ncmdn 1 1\nmovn ac 'l\ncmdn 1 1\nmovn ac 'o\ncmdn 1 1\nmovn ac ',\ncmdn 1 1\nmovn ac 'M\ncmdn 1 1\nmovn ac 'e\ncmdn 1 1\nmovn ac 'x\ncmdn 1 1\n");
+createFile("build.sh");
+writeFile("build.sh","echo building...\nmex hello\ncopy out.mex hello.mex\ndel out.mex\nmex ascii\ncopy out.mex ascii.mex\ndel out.mex\necho done\n");
+createFile("ascii");
+writeFile("ascii","movn ac 127\naddn ac 127\ncmdn 1 1\nsubn ac 1\nbign ac 1\njnzn 8 1\n");
+}
---
+++ src/System/MemoryManager.c
@@ -0,0 +1,31 @@
+#include"Memory.h"
+int last_index = 0xFFFFAA;
+char * new_str(int len){
+ int out = last_index;
+ char * temp = (char * )0x0;
+ for(int i = 0;i<len;i++){
+   temp[last_index+i] = 0;
+ }
+  last_index+=len;
+ return (char *)out;
+}
+void move_mem(int from,int to,int till){
+ char * from_c = (char *)0x0+from;
+ char * to_c  = (char *)0x0+to;
+ for(int i = 0;i<till;i++){
+  to_c[i]=from_c[i];
+ }
+}
+void memory_init(){
+  last_index = 0xFFFFAA;
+  char * tempr = (char *)last_index;
+  for(int i = 0;i<1000000;i++){
+     tempr[i]=0;
+  }
+}
+void roll_back(int to){
+  last_index-=to;
+}
+int getMemPointer(){
+  return last_index;
+}
