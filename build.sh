1cb1cf7fd145f47954a7002c55a6386160f2bc03
Gorian <1012176+Gorian@users.noreply.github.com>
2021-09-22 12:51:32 -0700

    Update build.sh
    Update build.sh to be idiomatic, shell-check compliant bash code

--- build.sh
+++ build.sh
@@ -1,26 +1,40 @@
-echo Cleaning
-mkdir -p bin
-rm -r -f bin/*.o
-echo Building
-nasm -f elf32 src/kernel_loader2.asm -o kernel_loader.o
-gcc -m32 -c -I src/. src/kernel.c -o kernel.o
-cd bin
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/Drivers/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/System/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/Graphics/*.c
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/Timer/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/FS/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/App32/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/Shell/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/Mego-Runtime/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/Terminal-Runtime/*.c 
-gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I ../src/. ../src/Util/*.c 
-ld -static -Bsymbolic -nostdlib -m elf_i386 -Ttext 0x100000 ../kernel_loader.o ../kernel.o  *.o  -o kernel.e
-rm ../*.o
-rm *.o
-objcopy -Felf32-i386 kernel.e kernel.elf
-rm *.e
-qemu-system-x86_64 -kernel kernel.elf
-cd ..
-rm -r -f bin
-echo Done
+#!/bin/bash
+
+die(){
+  printf "[ERROR] %s\n" "$1"
+  exit 1
+}
+
+main(){
+  local directories=(
+    "Drivers"
+    "System"
+    "Graphic"
+    "Timer"
+    "FS"
+    "App32"
+    "Shell"
+    "Mego-Runtime"
+    "Terminal-Runtime"
+    "Util"
+  )
+  printf "Cleaning\n"
+  mkdir -p "bin" >/dev/null 2>&1 || die "Could not make directory"
+  rm -rf "bin/"*.o
+  printf "Building\n"
+  nasm -f "elf32" "src/kernel_loader2.asm" -o "kernel_loader.o"
+  gcc -m32 -c -I "src/." "src/kernel.c" -o "kernel.o"
+  cd "bin" || die "Could not cd to bin"
+  for directory in "${directories[@]}"; do
+    gcc -m32 -fno-exceptions -ffreestanding -fshort-wchar -mno-red-zone -c -I "../src/." "../src/${directory}/"*.c
+  done
+  rm -rf -- "../"*.o || die "Could not remove object files"
+  rm -rf -- *.o || die "Could not remove object files"
+  objcopy -Felf32-i386 "kernel.e kernel.elf"
+  rm -rf -- *.e || die "Could not remove object files"
+  qemu-system-x86_64 -kernel "kernel.elf"
+  cd .. || die "Could not cd to previous directory"
+  rm -rf bin || die "Could not remove bin directory"
+}
+
+main "$@"
