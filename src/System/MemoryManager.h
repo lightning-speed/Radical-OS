a5cab6616034692fa37f9a724795688ee0adfc33
sheispeko <82322282+lightning-speed@users.noreply.github.com>
2021-09-25 16:09:21 +0530

    Delete src directory

--- src/System/MemoryManager.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-char * new_str(int len);
-void roll_back(int to);
-int getMemPointer();
--- src/Timer/Timer.c
+++
@@ -1,8 +0,0 @@
-#include"Timer.h"
-#include<System/SystemTime.h>
-void Sleep(int seconds){
-  for(int i = 0;i<seconds;i++){
-    int temp = getTimeFromBoot();
-    while(temp==getTimeFromBoot()){}
-  }
-}
