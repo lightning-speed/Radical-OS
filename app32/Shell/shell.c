e0f331563a06a2aa7ff9f033b2a84bd527fcd5ad
sheispeko <82322282+lightning-speed@users.noreply.github.com>
2021-11-01 10:11:21 +0530

    Add files via upload

--- app32/Shell/shell.c
+++ app32/Shell/shell.c
@@ -3,20 +3,33 @@
 #include <util_string.h>
 void start_program(char **args, int offset)
 {
+	int echo = 1;
 	changeColor(0x0f);
 	printS("\nStarted Shell!\n" - offset);
 	changeColor(0x0e);
 	char in[2000];
 	for (;;)
 	{
-		printS("\nuser@radical:# " - offset);
+		if (echo)
+			printS("\nuser@radical:# " - offset);
+		else
+			printChar('\n');
 		changeColor(0x0f);
 		scanS(in);
 		if (equal(in, "exit" - offset))
 		{
 			return;
 		}
-		system(in);
+		else if (equal(in, "@echo off" - offset))
+		{
+			echo = 0;
+		}
+		else if (equal(in, "@echo on" - offset))
+		{
+			echo = 1;
+		}
+		else
+			system(in);
 		changeColor(0x0e);
 		for (int i = 0; i < 2000; i++)
 		{
